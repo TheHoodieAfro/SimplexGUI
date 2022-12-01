5afe76e1b0f9c6f7c9ca3b4c31e5a4565bf3734b
Cristian Sanchez Pelaez <47835636+TheHoodieAfro@users.noreply.github.com>
2022-11-30 23:34:06 -0500

    update

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDWDO68PhUV3y7PL6CGPYqHH_Tk6UmUwm0",
-  authDomain: "simplexclassifier.firebaseapp.com",
-  databaseURL: "https://simplexclassifier-default-rtdb.firebaseio.com",
-  projectId: "simplexclassifier",
-  storageBucket: "simplexclassifier.appspot.com",
-  messagingSenderId: "84073702270",
-  appId: "1:84073702270:web:816a52b224ef3aa544892f",
-};
+const firebaseConfig = {}; // Ingresar la configuracion propia
 
 export const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
